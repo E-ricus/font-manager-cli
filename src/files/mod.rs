09e0f0564bf2eabf830ff19f96bac0c0a9ea25e1
Eric Puentes <eric.david2333@gmail.com>
2023-01-08 16:42:05 -0500

    rename username

--- src/files/mod.rs
+++ src/files/mod.rs
@@ -54,7 +54,7 @@ pub(crate) fn extract_fonts_from_zip(
             );
             if let Some(p) = outpath.parent() {
                 if !p.exists() {
-                    fs::create_dir_all(&p)?;
+                    fs::create_dir_all(p)?;
                 }
             }
             let mut outfile = fs::File::create(&outpath)?;
